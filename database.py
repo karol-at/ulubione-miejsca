ab5042ac4e48ccffbe54dc479cd133d647c84313
Karol Atys <atys@atys.nazwa.pl>
2025-03-07 15:10:19 +0100

    add type hints to place interaction methods

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+from flask import Request
 import sqlite3
 
-def add_place(request):
+def add_place(request: Request):
     return
 
-def get_places(sessionId: str):
+def get_places(sessionId: str) -> dict:
     return
