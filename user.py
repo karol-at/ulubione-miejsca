6ee3430801e2af65224790116a47d0efac871ecc
Genyra <m.sarzynska2@gmail.com>
2025-04-02 12:50:48 +0200

    add user registration

---
+++ user.py
@@ -0,0 +1,26 @@
+import sqlite3
+from typing import TypedDict
+from hashlib import sha256
+import time
+
+
+class user(TypedDict):
+    user_id: int
+    username: str
+    password: str
+    session_hash: str
+
+
+def register(args: user) -> str:
+    args["password"] = sha256(args["password"].encode()).hexdigest()
+    args["session_hash"] = str(time.time())
+    con = sqlite3.connect('baza.db')
+    cur = con.cursor()
+    cur.execute(f'''
+        INSERT INTO users VALUES  (
+                {args["user_id"]},
+                {args["username"]},
+                {args["password"]},
+                {args["session_hash"]}
+                )''')
+    return args["session_hash"]
