7d38702dd54b4d3150caf5a2d0e8a204d66509d1
Karol Atys <atys@atys.nazwa.pl>
2025-04-02 17:02:52 +0200

    add login and register form element access

---
+++ static/login.js
@@ -0,0 +1,11 @@
+const loginForm = {
+  username: document.getElementById("login-username"),
+  password: document.getElementById("login-password"),
+  submit: document.getElementById("login-submit"),
+};
+const registerForm = {
+  username: document.getElementById("register-username"),
+  password: document.getElementById("register-password"),
+  repassword: document.getElementById("register-repassword"),
+  submit: document.getElementById("register-submit"),
+};
