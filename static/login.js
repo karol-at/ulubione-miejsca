bf8089ee8f9955547a16e1176e0543ec099483c9
Karol Atys <atys@atys.nazwa.pl>
2025-04-02 17:19:44 +0200

    add basic registering logic

--- static/login.js
+++ static/login.js
@@ -9,3 +9,31 @@ const registerForm = {
   repassword: document.getElementById("register-repassword"),
   submit: document.getElementById("register-submit"),
 };
+
+//TODO: Add proper handling for invalid inputs
+registerForm.submit.onclick = async (e) => {
+  if (
+    registerForm.username.value == "" ||
+    registerForm.password.value == "" ||
+    registerForm.repassword.value == ""
+  ) return;
+  if (registerForm.password.value != registerForm.repassword.value) return;
+
+  const res = await fetch(
+    "/register",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        //TODO: Remove the need for user_id on the backend
+        user_id: 1,
+        username: registerForm.username.value,
+        password: registerForm.password.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const data = await res.json();
+  //TODO: Add response handling
+};
