2bf35e852828665e8e85756b384443791d32d2ce
Genyra <m.sarzynska2@gmail.com>
2025-04-02 11:53:48 +0200

    implement creating database on start

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from flask import Flask, render_template, request
 import database
+import sqlite3
+
+con = sqlite3.connect('baza.db')
+cur = con.cursor()
+cur.execute('''
+            CREATE TABLE IF NOT EXIST users(
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            password TEXT,
+            session_hash TEXT 
+            )''')
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/places', methods=['GET','POST'])
+
+@app.route('/api/places', methods=['GET', 'POST'])
 def places():
     if request.method == 'POST':
         database.add_place(request)
     elif request.method == 'GET':
         database.get_places(request.cookies.get('sessionId'))
 
+
 @app.post('/register')
 def register():
-    return 
+    return
