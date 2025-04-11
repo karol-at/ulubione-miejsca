81b9a5f9274c92c140ec4420ea5e204b21f16d83
karol-at <karol.atys@gmail.com>
2025-04-11 15:13:15 +0200

    remove unused function definitions

--- database.py
+++ database.py
@@ -2,14 +2,6 @@ from flask import Request
 import sqlite3
 
 
-def add_place(request: Request):
-    return
-
-
-def get_places(sessionId: str) -> dict:
-    return
-
-
 def execute_query(sql: str, args: tuple) -> list[any]:
     con = sqlite3.connect('baza.db')
     cur = con.cursor()
