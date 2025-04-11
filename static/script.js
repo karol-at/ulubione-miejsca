c0aa09aa17a5bfbadc053d06107bab097be14ec5
karol-at <karol.atys@gmail.com>
2025-04-11 12:20:38 +0200

    add dialog for creating new places

--- static/script.js
+++ static/script.js
@@ -7,3 +7,16 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 const menu = document.querySelector("#scroll");
 const template = document.querySelector("#location");
+const newPlaceWrapper = document.getElementById("new-place-wrapper");
+const newPlaceForm = {
+    dialog: document.getElementById("new-place-dialog"),
+    name: document.getElementById("place-name"),
+    submit: document.getElementById("place-submit"),
+};
+
+document.onclick = (e) => {if (e.target.contains(newPlaceForm.dialog)) newPlaceForm.dialog.close()  }
+
+
+map.on("click", async (e) => {
+    newPlaceForm.dialog.showModal()
+})
