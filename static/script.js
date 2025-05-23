54cccc5ad287ec83c6f5523bb887e337afbbcc9b
karol-at <karol.atys@gmail.com>
2025-05-23 13:20:16 +0200

    add place deletion

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-let map = L.map("map").setView([51.505, -0.09], 13);
+let map = L.map("map");
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
@@ -48,9 +48,10 @@ newPlaceForm.submit.onclick = async () => {
                 "Content-Type": "application/json",
             },
         },
-    );
+    ).then((res) => res.json());
     if (res.status == "ok") {
         createPlace(place);
+        newPlaceForm.dialog.close();
     }
 };
 
@@ -64,9 +65,11 @@ async function displayPlaces() {
      * @type {Place}
      */
     const places = await fetch("/places").then((res) => res.json());
-    if (places.lenght == 0) {
+    if (places.length == 0) {
+        map.setView([51.5, 0], 13);
         throw "error";
     }
+    map.setView([places[0].latitude, places[0].longitude], 13);
     places.forEach((element) => {
         createPlace(element);
     });
@@ -81,7 +84,20 @@ function createPlace(place) {
     placesHTML.push(
         currentPlace,
     );
-    currentPlace.querySelector(".location").innerHTML = place.name;
+    currentPlace.querySelector(".location-title").innerHTML = place.name;
+    currentPlace.querySelector(".location-delete").onclick = (e) => {
+        e.target.parentNode.remove();
+        placesLeaflet.filter((v) => v.options.title === place.name)[0].remove();
+        fetch("/places", {
+            method: "DELETE",
+            body: JSON.stringify({
+                place_id: place.id,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    };
     menu.appendChild(currentPlace);
     placesLeaflet.push(
         L.marker([place.latitude, place.longitude], { title: place.name })
@@ -90,3 +106,5 @@ function createPlace(place) {
 }
 
 displayPlaces().catch();
+
+console.log(placesLeaflet);
